0fe70ff7bad70e7b2c5e80ae8fa58bf76bfb2437
Aviv Eyal <aviveyal07@gmail.com>
2020-06-28 13:38:48 +0300

    basic errors checking

--- main.go
+++ main.go
@@ -34,17 +34,27 @@ func main() {
 	var sm SignedMessage
 	json.Unmarshal([]byte(data), &sm)
 	fmt.Printf("Message: %s, Signature: %s\n", sm.Text, sm.Signature)
+
 	msg := []byte(sm.Text)
-	sig, _ := hex.DecodeString(sm.Signature[2:])
-	pub, _ := ed25519.ExtractPublicKey(msg, sig)
-	account2 := pub[12:]
+	sig, err := hex.DecodeString(sm.Signature[2:])
+	if err != nil {
+		fmt.Println("Invalid signature format:", sm.Signature)
+		return
+	}
+
+	pub, err := ed25519.ExtractPublicKey(msg, sig)
+	if err != nil {
+		fmt.Println("Can't extract public key from message and signature.", err)
+		return
+	}
+	account := pub[12:]
 
 	// verify the signature
 	verified := ed25519.Verify2(pub, msg, sig)
 	if verified == true {
-		fmt.Printf("Valid signature. Message: %s. Account: 0x%x\n", msg, account2 )
+		fmt.Printf("Valid signature. Message: %s. Account: 0x%x\n", msg, account )
 	} else {
-		fmt.Printf("Invalid signature. Message: %s. Account: 0x%x\n", msg, account2 )
+		fmt.Printf("Invalid signature. Message: %s. Account: 0x%x\n", msg, account )
 	}
 
 }
